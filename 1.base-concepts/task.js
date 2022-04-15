866ef81904a2d0f572c87b4494985ac43fb90457
SilantevR <craft_89@inbox.ru>
2022-04-15 13:15:03 +0300

    task 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -15,8 +15,30 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
-  // код для задачи №2 писать здесь
+  if (isNaN(percent) || Number(percent) <= 1 || Number(percent) > 100) {
+    totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(contribution) || Number(contribution) < 0) {
+    totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (
+    isNaN(amount) ||
+    Number(amount) <= 0 ||
+    Number(amount) < Number(contribution)
+  ) {
+    totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  } else {
+    let S = Number(amount) - Number(contribution);
+    let P = percent / 100 / 12;
+    let today = new Date();
+    let creditStartYear = today.getFullYear();
+    let creditStartMonth = today.getMonth();
+    let creditEndYear = date.getFullYear();
+    let creditEndMonth = date.getMonth();
+    let n =
+      (creditEndYear - creditStartYear) * 12 +
+      (creditEndMonth - creditStartMonth);
+    let payment = S * (P + P / ((1 + P) ** n - 1));
+    totalAmount = Number((payment * n).toFixed(2));
+  }
 
   return totalAmount;
 }
