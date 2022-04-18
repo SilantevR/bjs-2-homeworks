8694e31255f98c307436a001a92f91b325b4b1dd
SilantevR <craft_89@inbox.ru>
2022-04-18 14:29:23 +0300

    task 1 + task 2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (arr1.length < arr2.length) {
+    arr2.every((element, idx) => (result = element === arr1[idx]));
+  } else {
+    arr1.every((element, idx) => (result = element === arr2[idx]));
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr
+    .filter((item) => item >= 0)
+    .filter((item) => item % 3 === 0)
+    .map((item) => (item *= 10));
 
   return resultArr; // array
 }
