7a5a2a76a03d745b31c1f673b82d3f62a2d72103
SilantevR <craft_89@inbox.ru>
2022-04-21 14:29:15 +0300

    task 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
+"use strict";
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark); // добавить вторую и последующие оценки в массив
+  }
+};
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = [...marks];
+  } else {
+    this.marks.push(...marks); // добавить вторую и последующие оценки в массив
+  }
+};
+
+Student.prototype.getAverage = function () {
+  let sum;
+  if (this.marks !== undefined) {
+    sum = 0;
+    this.marks.keys();
+    for (let keys of this.marks) {
+      sum += keys;
+    }
+  }
+  return sum / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
 // ваш код для остальных методов
