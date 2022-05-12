ba437ef9b255420116cc6a4dcc651a813f4fa5f5
SilantevR <craft_89@inbox.ru>
2022-05-12 11:42:06 +0300

    task 1 + task 2 + task 3

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,63 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+  return function wrapper(...args) {
+    const hash = args.join(",");
+    if (hash in cache) {
+      console.log("Из кэша: " + cache[hash]);
+      return "Из кэша: " + cache[hash];
+    } else {
+      let result = func.call(this, ...args);
+      let keys = Object.keys(cache);
+      if (keys.length >= 5) {
+        delete cache[keys[0]];
+      }
+      cache[hash] = result;
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  };
 }
 
+function debounceDecoratorNew(func, ms) {
+  let timeout;
+  let timer;
+  let timerForLast;
+  //let count = 0;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  return function () {
+    //console.log(++count);
+    clearTimeout(timer);
+    clearTimeout(timerForLast);
+    timer = setTimeout(() => {
+      timeout = false;
+      timerForLast = setTimeout(func, ms);
+    }, ms);
+    if (timeout) {
+      return;
+    }
+    func();
+    timeout = true;
+  };
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timeout;
+  let timer;
+  let timerForLast;
+  let count = 0;
+
+  return function () {
+    console.log(++count);
+    clearTimeout(timer);
+    clearTimeout(timerForLast);
+    timer = setTimeout(() => {
+      timeout = false;
+      timerForLast = setTimeout(func, ms);
+    }, ms);
+    if (timeout) {
+      return;
+    }
+    func();
+    timeout = true;
+  };
 }
